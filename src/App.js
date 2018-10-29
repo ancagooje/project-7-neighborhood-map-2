d1436511bf937e334983f9bdc2162245225ae9d8
Anca Gooje <ancagooje@gmail.com>
2018-10-29 16:27:17 -0400

    added map component

--- src/App.js
+++ src/App.js
@@ -1,109 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Components/Map'
-import Filter from './Components/Filter'
-import Sidebar from './Components/Sidebar'
-import axios from 'axios'
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-this.state = {
-  venues: [],
-  markers: [],
-}
-  }
-
-  componentDidMount() {
-    this.getVenues()
-  }
-
-  loadMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCRlDr_ACA80UcW_svxFgZqtUkzhL6COqI&callback=initMap")
-  window.initMap = this.initMap
-  }
-  
-  getVenues = () => {
-   const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-   const parameters = {
-     client_id: "RXELH2QF5VFRWCD11BWSZWE1TP3OEDXDY3FSN1U552FCOY0J",
-     client_secret: "X0O4RURKYRVBH0IXWDZRLUWT0ZPKWQXIDJXB3ANT3T44111N",
-     query: "seafood",
-     near: "Scarborough, ME",
-     v: "20182507"
-   }
-   axios.get(endPoint + new URLSearchParams(parameters))
-   .then(response => {
-    this.setState({
-     venues: response.data.response.groups[0].items },
-      this.loadMap()) 
-   })
-   .catch(error => {
-     console.log("Error! " + error)
-   })
-  }
-
-  initMap = ()  => {
-
-    // display the Google map
-    const map = new window.google.maps.Map(document.getElementById('map'), 
-    { center: {lat: 43.5780556, lng: -70.3222222},
-      zoom: 11
-    });
-//created an Info Window using the example at https://developers.google.com/maps/documentation/javascript/infowindows
-  
-
-    this.state.venues.map(myVenue => {
-
-      const infowindow = new window.google.maps.InfoWindow();
-      
-      var contentString = '${myVenue.venue.name.phone.address}'
-      var contentString = `${myVenue.venue.name}`
-    // var contentString = `${myVenue.venue.phone}`
-//created a dynamic Marker using the example from https://developers.google.com/maps/documentation/javascript/markers
-      const marker = new window.google.maps.Marker({
-        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
-        map: map,
-
-        animation: window.google.maps.Animation.DROP,
-        
-        title: myVenue.venue.name
-      })
+import MapRendering from './components/MapRendering';
+import venues from './data/venues.json';
+import {InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
  
-      //event listener to open info window when clicking the market
-      marker.addListener('click', function() {
-
-        //change the content for the info window with details for each venue
-        infowindow.setContent(contentString)
-
-        //open the info window
-        infowindow.open(map, marker);
-      });
-    })
-    
-    //In the case of any error
-   // .catch(error => {
-      // and pass them to handleError()
-    //  this.handleError(error)
-  //})
-
+class App extends Component {
+  state = {
+    lat: 43.5780556,
+    lon: -70.3222222,
+    zoom: 13,
+    all: venues
   }
-  render() {
+ 
+ 
+
+  render = () => {
     return (
-       <main>
-         <div id="map"></div>
-       </main>
-    )
+      <div className="App">
+        <div>
+          <h1> Seafood in Scarborough, ME </h1>
+        </div>
+        <MapRendering
+          lat={this.state.lat}
+          lon={this.state.lon}
+          zoom={this.state.zoom}
+          locations={this.state.all}/>
+      </div>
+    );
   }
 }
 
-function loadScript(url) {
-var index = window.document.getElementsByTagName("script")[0]
-var script = window.document.createElement("script")
-script.src = url
-script.async = true
-script.defer = true
-index.parentNode.insertBefore(script, index)
-}
 export default App;
+   
+//export default GoogleApiWrapper({
+  //apiKey: (AIzaSyCRlDr_ACA80UcW_svxFgZqtUkzhL6COqI)})(MapContainer)
+  
+  
+   
+  
---
+++ src/components/MapRendering.js
@@ -0,0 +1,45 @@
+
+import React, {Component} from 'react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
+
+const MAP_KEY = "AIzaSyCRlDr_ACA80UcW_svxFgZqtUkzhL6COqI";
+
+class MapRendering extends Component {
+    state = {
+        map: null
+    };
+
+    componentDidMount = () => {
+    }
+
+    mapReady = (props, map) => {
+        // Save the map reference in state and prepare the location markers
+        this.setState({map});
+    }
+
+    render = () => {
+        const style = {
+            width: '100%',
+            height: '100%'
+        }
+        const center = {
+            lat: this.props.lat,
+            lng: this.props.lon
+        }
+
+        return (
+            <Map
+                role="application"
+                aria-label="map"
+                onReady={this.mapReady}
+                google={this.props.google}
+                zoom={this.props.zoom}
+                style={style}
+                initialCenter={center}
+                onClick={this.closeInfoWindow}>
+            </Map>
+        )
+    }
+}
+
+export default GoogleApiWrapper({apiKey: MAP_KEY})(Map)
